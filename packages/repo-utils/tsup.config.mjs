f7f1baf9fcae52c2c86bf38d57209bd1107bc1ac
Tony Wu <tonywu6@protonmail.com>
2023-09-26 18:17:17 +0800

    feat: add experimental repo-utils (#3)
    * feat: add experimental repo-utils
    
    * feat: add experimental repo-utils

--- packages/repo-utils/tsup.config.mjs
+++ packages/repo-utils/tsup.config.mjs
@@ -6,11 +6,13 @@ export default [
   defineConfig({
     format: ['esm'],
     entry: ['./src/index.mts'],
+    outDir: './dist',
     outExtension: () => ({ js: '.mjs' }),
   }),
   defineConfig({
     format: ['esm'],
-    entry: ['./src/cli.mts'],
+    entry: ['./src/cli/index.mts'],
+    outDir: './dist/cli',
     outExtension: () => ({ js: '.mjs' }),
   }),
 ];
